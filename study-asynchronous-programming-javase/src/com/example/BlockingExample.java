42494d9954561b552cdc019ffa603405bbd8c1c8
deepcloudlabs <deepcloudlabs@gmail.com>
2020-08-27 19:09:32 +0300

    remove warnings

--- study-asynchronous-programming-javase/src/com/example/BlockingExample.java
+++ study-asynchronous-programming-javase/src/com/example/BlockingExample.java
@@ -10,28 +10,29 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public class BlockingExample {
 
 	public static void main(String[] args) throws InterruptedException {
 		var numbers = Collections.synchronizedList(new ArrayList<List<Integer>>());
-		Function<List<Integer>,List<Integer>> fn = 
-		lotteryNumbers -> {
-			System.err.println(Thread.currentThread().getName()+": "+lotteryNumbers); 
+		Function<List<Integer>, List<Integer>> fn = lotteryNumbers -> {
+			System.err.println(Thread.currentThread().getName() + ": " + lotteryNumbers);
 			return lotteryNumbers;
 		};
-		for (var i=0;i<10;++i)
-			 draw().thenApplyAsync(fn);// 1sec
-		//numbers.forEach(System.out::println);
+		for (var i = 0; i < 10; ++i)
+			draw().thenApplyAsync(fn);// 1sec
+		// numbers.forEach(System.out::println);
 		TimeUnit.SECONDS.sleep(100);
 	}
 
-	public static CompletableFuture<List<Integer>> draw(){
+	public static CompletableFuture<List<Integer>> draw() {
 		return CompletableFuture.supplyAsync(() -> {
 			try {
 				TimeUnit.SECONDS.sleep(1); // IO (Network/Disk IO)
-			} catch (InterruptedException e) { }
+			} catch (InterruptedException e) {
+			}
 			return ThreadLocalRandom.current().ints(1, 50).distinct().limit(6).sorted().boxed().collect(toList());
 		});
-		
+
 	}
 }
