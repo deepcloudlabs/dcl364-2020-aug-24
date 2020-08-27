6ed33c7b29ffe227968180a94d2513d2a449c3af
deepcloudlabs <deepcloudlabs@gmail.com>
2020-08-27 11:19:50 +0300

    initial commit

--- stockmarket-spring-boot-jax-rs/src/main/java/com/example/stockmarket/StockmarketSpringBootApplication.java
+++ stockmarket-spring-boot-jax-rs/src/main/java/com/example/stockmarket/StockmarketSpringBootApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// Java EE -> WAR (Application Code) -> 7KB
+// Spring Boot (Application Code + Embedded Container )-> Fat JAR -> 45.7 MB
+   // mvn clean install spring-boot:repackage
+// Containerization (Docker) Dockerfile -> COPY WAR (
 @SpringBootApplication
 @EnableScheduling
 public class StockmarketSpringBootApplication {
